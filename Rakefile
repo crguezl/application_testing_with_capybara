d05a8ee2f075ebffe00c43df55963671d0798b9b
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-26 14:09:10 +0000

    operative for Sinatra example

---
+++ Rakefile
@@ -0,0 +1,4 @@
+task :default => :rackfeatures
+task :rackfeatures do
+  sh "cucumber -r features features/chapter3/sinatra.feature "
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,11 +3,12 @@ require 'capybara/cucumber'
 require 'rspec/expectations'
 require_relative '../../sinatra/app'
 
-Capybara.default_driver = :selenium
-#Capybara.default_driver = :rack_test #Use this when looking at the examples in chapter 3 and compare it with using Selenium!
+#Capybara.default_driver = :selenium
+Capybara.default_driver = :rack_test #Use this when looking at the examples in chapter 3 and compare it with using Selenium!
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
-end
+#Capybara.register_driver :selenium do |app|
+#  #Capybara::Selenium::Driver.new(app, :browser => :firefox)
+#  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
 
 Capybara.app = BookReview #our Sinatra app
